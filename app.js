bf6c07dfad47a8ed0cb6d7088ca603b68ddd2ad9
Am1d1v <dimadiman1adima@gmail.com>
2024-03-13 20:16:07 +0300

    Styles added.

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 
 
 
+// Enemy Class. Enemies behavior
+class Enemy {
+
+}
+
+// Game Class. Logic of the game
+class Game {
+    constructor(canvas){
+        this.canvas = canvas;
+        this.width = this.canvas.width;
+        this.height = this.canvas.height;
+    }
+}
+
+window.addEventListener('load', () => {
+    const canvas = document.querySelector('#canvas1');
+    const context = canvas.getContext('2d');
+
+    // Set Full width and Height
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+
+})
